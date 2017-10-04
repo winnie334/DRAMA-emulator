34d44fb5c32c758a5729fc2e3c92eeb24580b68c
Arthur Bols <arthur.bols@outlook.com>
2017-10-04 15:20:42 +0200

    Created solution and basic gui

---
+++ DRAMA_Emulator_GUI/DRAMA_Emulator_GUI/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DRAMA_Emulator_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonRun_Click(object sender, EventArgs e)
+        {
+            //Do stuff
+            ReadTextBox();
+        }
+
+
+        private void ReadTextBox()
+        {
+            string[] textLines = CodeText.Text.Split('\n');
+            foreach(var line in textLines)
+            {
+
+            }
+        }
+    }
+}
